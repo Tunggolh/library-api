6de47d0d89b75f67ecafe514d01ba881c8529702
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-08 17:58:28 +0800

    Add author module,service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/typeorm.config';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...dbConfig,
     }),
+    AuthorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/authors/authors.spec.ts
@@ -0,0 +1,7 @@
+import { Authors } from './authors';
+
+describe('Authors', () => {
+  it('should be defined', () => {
+    expect(new Authors()).toBeDefined();
+  });
+});
