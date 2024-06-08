abe1e0d4d256351cb6437ac7ae8f71a1ec45421e
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-08 17:57:32 +0800

    Add initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './config/typeorm.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      ...dbConfig,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
