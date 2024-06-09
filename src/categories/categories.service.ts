d7b956dd38ca570b9e4097d6b002208a38f37cf2
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-09 13:12:25 +0800

    Add category service logic

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private readonly categoryRepository: Repository<Category>) {}
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find({ order: { id: 'asc' } });
+  }
+
+  async findOne(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+
+    if (!category) {
+      throw new NotFoundException(`Category #${id} not found`);
+    }
+
+    return category;
+  }
+
+  async create(categoryDto: CategoryDto): Promise<Category> {
+    return this.categoryRepository.save(categoryDto);
+  }
+
+  async update(id: number, categoryDto: CategoryDto): Promise<Category> {
+    const category = await this.categoryRepository.preload({
+      id,
+      ...categoryDto,
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category #${id} not found`);
+    }
+
+    return this.categoryRepository.save(category);
+  }
+
+  async remove(id: number): Promise<void> {
+    const category = await this.findOne(id);
+    this.categoryRepository.remove(category);
+  }
+}
