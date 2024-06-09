773eed2d82142737087e2b0012db5d114578a3dd
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-09 12:43:15 +0800

    Add initial category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 import { dataSourceOptions } from './config/typeorm.config';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), AuthorsModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), AuthorsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
