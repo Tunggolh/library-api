33653d9b2f959f47e64f66ab03137bc4b99f32f4
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-09 15:18:30 +0800

    Add book dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,15 @@
-export class CreateBookDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly isbn: string;
+
+  @IsNumber()
+  readonly authors: number[];
+
+  @IsNumber()
+  readonly categories: number[];
+}
