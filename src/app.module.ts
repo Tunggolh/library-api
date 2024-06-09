ced6547d3441af292f55da71513b92b701c93672
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-09 13:31:07 +0800

    Add books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 import { dataSourceOptions } from './config/typeorm.config';
 import { CategoriesModule } from './categories/categories.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     AuthorsModule,
     CategoriesModule,
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
