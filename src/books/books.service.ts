3561646409320447882be402c294bcc0be90018b
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-09 13:57:37 +0800

    Add book service logic

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>,
+  ) {}
+
+  async findAll(paginationQuery): Promise<Book[]> {
+    return this.bookRepository.find();
+  }
+
+  async findOne(id: number): Promise<Book> {
+    const book = this.bookRepository.findOne({
+      where: { id },
+      relations: ['authors', 'categories'],
+    });
+
+    if (!book) {
+      throw new NotFoundException(`Book #${id} not found`);
+    }
+
+    return book;
+  }
+
+  async create(createBookDto: CreateBookDto): Promise<Book> {
+    return this.bookRepository.save(createBookDto);
+  }
+
+  async update(id: number, updateBookDto: UpdateBookDto): Promise<Book> {
+    const book = await this.bookRepository.preload({
+      id,
+      ...updateBookDto,
+    });
+
+    if (!book) {
+      throw new NotFoundException(`Book #${id} not found`);
+    }
+
+    return this.bookRepository.save(book);
+  }
+
+  async remove(id: number) {
+    const book = await this.findOne(id);
+    return this.bookRepository.remove(book);
+  }
+}
